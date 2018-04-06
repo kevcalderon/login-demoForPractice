ec27293e3eba5c5fa2b7180e4f9c9ad527af436b
kevincp29 <josue_29c@hotmail.com>
2018-04-05 21:05:08 -0600

    Include firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	production: true,
+	firebaseConfig:{
+		apiKey: "AIzaSyDrIOIIsG8V-YQ8RN2VcyXQJgMO2NBA72I",
+		authDomain: "loginapp-7913f.firebaseapp.com",
+		databaseURL: "https://loginapp-7913f.firebaseio.com",
+		projectId: "loginapp-7913f",
+		storageBucket: "",
+		messagingSenderId: "883247122443"
+
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebaseConfig:{
+		apiKey: "AIzaSyDrIOIIsG8V-YQ8RN2VcyXQJgMO2NBA72I",
+		authDomain: "loginapp-7913f.firebaseapp.com",
+		databaseURL: "https://loginapp-7913f.firebaseio.com",
+		projectId: "loginapp-7913f",
+		storageBucket: "",
+		messagingSenderId: "883247122443"
+
+	}
 };
