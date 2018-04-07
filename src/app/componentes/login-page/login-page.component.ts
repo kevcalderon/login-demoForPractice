e640398572fa1f588361cbfdeeea47ee34a59df0
kevincp29 <josue_29c@hotmail.com>
2018-04-06 21:16:05 -0600

    login navbar exit and login

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+	public email:string;
+	public password:string;
+	constructor(
+		public authService: AuthService,
+		public router: Router
+		) { }
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	onSubmitLogin(){
+		this.authService.loginUser(this.email, this.password)
+		.then((res)=>{
+			this.router.navigate(['/privado']);
+		}).catch((err)=>{
+			console.log(err);
+			this.router.navigate(['/login']);	
+		});
+	}
 }
--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../servicios/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+	public isLogin: boolean;
+	public nombreUsuario: string;
+	public emailUsuario:string;
+  constructor(
+  	public authService: AuthService
+  	) { }
 
   ngOnInit() {
+  	this.authService.getAuth().subscribe( autho=>{
+  		if (autho) {
+  			this.isLogin = true;
+  			this.nombreUsuario = autho.displayName;
+  			this.emailUsuario = autho.email;
+  		} else {
+  			this.isLogin = false;
+  		}
+  	})
+  }
+
+  onClickLogout(){
+  	this.authService.logout();
   }
 
 }
